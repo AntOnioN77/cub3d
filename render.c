01d717ef754236b1c2b2bdbe32e29c968dc97566
Ant <you@example.com>
2025-07-11 13:41:25 +0200

    calculate distances arroja resultados plausibles en test unitarios

--- render.c
+++ render.c
@@ -6,7 +6,7 @@
 /*   By: antofern <antofern@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 14:09:58 by antofern          #+#    #+#             */
-/*   Updated: 2025/07/10 20:38:28 by antofern         ###   ########.fr       */
+/*   Updated: 2025/07/11 13:40:40 by antofern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,45 +67,15 @@ void draw_image(t_world *world, t_vector screen_vectors[])
 	//print_column(distances, world->data, world->textures);
 }
 
-void calculate_distances(t_world *world, const t_vector vectors[], double distances[], t_wall *wall)
-{
-	int i;
-
-	i = 0;
-	while(i < WINDOW_WIDTH)
-	{
-		distances[i] = one_ray(world->map, &(world->char_position) , &(vectors[i]), wall[i]);
-		i++;
-	}
-}
-
-
-void init_ray(t_vector *char_position, t_vector *vector, t_ray *ray)
-{
-	//En que baldosa del mapa comienza el rayo (su valor se ira incrementando al lo largo del DDA):
-	ray->tile_x = (int)char_position->x;
-	ray->tile_y = (int)char_position->y;
-	//A lo largo del rayo, cual es la distancia entre dos lineas del mismo eje:
-	if (vector->x == 0)
-		ray->delta_dist_x = MAX_RAY_DISTANCE;
-	else
-		ray->delta_dist_x = fabs(1 / vector->x);
-	if (vector->y == 0)
-		ray->delta_dist_y = MAX_RAY_DISTANCE;
-	else
-		ray->delta_dist_y = fabs(1 / vector->x);
-	//Cuanto falta hasta la proxima linea ray->side_dist_ :
-	calc_side_dist(ray, char_position, vector);
-}
 
 // ray->side_dist_x:Cuanto falta hasta la proxima linea
 // ray->step_x:nos mobemos a delante(1) o acia detras(-1) en x
-void calc_side_dist(t_ray *ray, t_vector *char_position, t_vector *vector)
+void calc_side_dist(t_ray *ray, t_vector *char_position, const t_vector *vector)
 {
 	if (vector->x > 0)
 	{
 		ray->step_x = 1;
-		ray->side_dist_x = (char_position->x + 1.0 - ray->tile_x) * ray->delta_dist_x; 
+		ray->side_dist_x = (ray->tile_x + 1.0 - char_position->x ) * ray->delta_dist_x; 
 	}
 	else
 	{
@@ -115,7 +85,7 @@ void calc_side_dist(t_ray *ray, t_vector *char_position, t_vector *vector)
 	if (vector->y > 0)
 	{
 		ray->step_y = 1;
-		ray->side_dist_y = (char_position->y + 1.0 - ray->tile_y) * ray->delta_dist_y; 
+		ray->side_dist_y = ( ray->tile_y  + 1.0 - char_position->y) * ray->delta_dist_y; 
 	}
 	else
 	{
@@ -125,37 +95,89 @@ void calc_side_dist(t_ray *ray, t_vector *char_position, t_vector *vector)
 }
 
 
+void init_ray(t_vector *char_position, const t_vector *vector, t_ray *ray)
+{
+	//En que baldosa del mapa comienza el rayo (su valor se ira incrementando al lo largo del DDA):
+	ray->tile_x = (int)char_position->x;
+	ray->tile_y = (int)char_position->y;
+	//A lo largo del rayo, cual es la distancia entre dos lineas del mismo eje:
+	if (vector->x == 0.0)
+		ray->delta_dist_x = MAX_RAY_DISTANCE;
+	else
+		ray->delta_dist_x = fabs(1 / vector->x);
+	if (vector->y == 0.0)
+		ray->delta_dist_y = MAX_RAY_DISTANCE;
+	else
+		ray->delta_dist_y = fabs(1 / vector->y);
+	//Cuanto falta hasta la proxima linea ray->side_dist_ :
+	calc_side_dist(ray, char_position, vector);
+}
+
+
 //OPTIMIZACION: en lugar de pasar t_vector, pasamos (t_vector *) un solo puntero
 // es mas economico que copiar una estructura con dos doubles
-double	one_ray(const char **map, t_vector *char_position, t_vector *vector, t_wall *wall)
+double	one_ray(const char **map, t_vector *char_position, const t_vector *vector, t_wall *wall, t_world *world)
 {
 	t_ray ray;
 
+
 	init_ray(char_position, vector, &(ray));
+printf("side_dist_x:%f, side_dist_y:%f\n", ray.side_dist_x, ray.side_dist_y);
+printf("delta_dist_x:%f, delta_dist_y:%f\n", ray.delta_dist_x, ray.delta_dist_y);
 	while(1)
 	{
-		//NO PROTEJO ACCESO A MEMMORIA INEXISTENTE (mucho lio, y se supone que son mapas cerrados por 1)
-		//anque las mates nos podrian llevar a escenarios donde sea un problema. Ya veremos.
-		//if rayo colisiona muro:
-		if(map[ray.tile_y][ray.tile_x] == 1)
+		if(ray.tile_x < 0 || ray.tile_y < 0 || ray.tile_x >= (world->map_width) || ray.tile_x >= (world->map_height))
 		{
-			if(wall == VERTICAL)
+printf("map[%d][%d] ha explotado.\n", ray.tile_y, ray.tile_x);
+			*wall = 7;
+			return 7470000.747;
+		}
+//printf("map[%d][%d] == %c.\n", ray.tile_y, ray.tile_x, map[ray.tile_y][ray.tile_x]);
+		if(map[ray.tile_y][ray.tile_x] == '1')
+		{
+			if(*wall == VERTICAL)
+			{
+printf("ray.tile_x - char_position->x + (1 - ray.step_x) / 2) / vector->x \n %d - %f + (1 - %d) / 2) / %f\n", ray.tile_x, char_position->x, ray.step_x, vector->x);
 				return((ray.tile_x - char_position->x + (1 - ray.step_x) / 2) / vector->x);
-			if(wall == HORIZONTAL)
+			}
+			if(*wall == HORIZONTAL)
+			{
+printf("ray.tile_y - char_position->y + (1 - ray.step_y) / 2) / vector->y \n %d - %f + (1 - %d) / 2) / %f\n", ray.tile_y, char_position->y, ray.step_y, vector->y);
 				return((ray.tile_y - char_position->y + (1 - ray.step_y) / 2) / vector->y);
+
+			}
 		}
 		//incrementamos distancia alternativamente side_dist_x side_dist_y
 		if(ray.side_dist_x < ray.side_dist_y)
 		{
+printf("     ||VERTICAL||\nray.step_x:%d, ray.tile_x:%d, ray.side_dist_x:%f\n", ray.step_x, ray.tile_x, ray.side_dist_x);
+
 			ray.side_dist_x += ray.delta_dist_x;
 			ray.tile_x += ray.step_x;
 			*wall = VERTICAL;
 		}
 		else
 		{
+printf("     --HORIZONTAL--\n ray.step_y:%d, ray.tile_y:%d, ray.side_dist_y:%f\n", ray.step_y, ray.tile_y, ray.side_dist_y);
+
 			ray.side_dist_y += ray.delta_dist_y;
 			ray.tile_y += ray.step_y;
 			*wall = HORIZONTAL;
 		}
+	}
+}
+
+
+void calculate_distances(t_world *world, const t_vector vectors[], double distances[], t_wall *wall)
+{
+	int i;
+
+	i = 0;
+	while(i < WINDOW_WIDTH)
+	{
+printf("-------------distance[%d]-----------\n",i);
+		distances[i] = one_ray((const char**)world->map, &(world->char_position) , &(vectors[i]), &(wall[i]), world);
+printf("	distance[%d] == %f\n",i, distances[i]);
+		i++;
 	}
 }
