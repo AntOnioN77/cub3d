41c8da7b5836de745939992b14e6054752e4fb0b
Ant <you@example.com>
2025-08-05 10:56:14 +0200

    Implementado slide, ahorasignamos un volumen al personaje, antes de ocupar una nueva posicion comprobamos que ninguna porcion del personaje queda solapada en un muro

--- src/char_movement.c
+++ src/char_movement.c
@@ -6,7 +6,7 @@
 /*   By: antofern <antofern@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 16:50:51 by antofern          #+#    #+#             */
-/*   Updated: 2025/07/30 10:16:32 by antofern         ###   ########.fr       */
+/*   Updated: 2025/08/05 10:49:16 by antofern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 void	char_movement(t_world *world)
 {
 	t_vector	new_pos;
-
 	new_pos = world->char_position;
+	
 	if (world->key_down['d'])
 	{
 		rotate_vector(&world->char_direction, ROT_SIN, ROT_COS);
@@ -40,55 +40,57 @@ void	char_movement(t_world *world)
 	set_position(new_pos.x, new_pos.y, world);
 }
 
-void	set_position(double new_position_x, double new_position_y,
-	t_world *world)
+int	is_wall(double x, double y, char **map)
 {
-	double	margin_x;
-	double	margin_y;
+	return (map[(int)y][(int)x] == '1');
+}
 
-	if ((int)new_position_y < (world->map_height)
-		&& (int)new_position_x < (world->map_width)
-		&& new_position_y >= 0 && new_position_x >= 0)
+//No verifica un punto, sino un area alrededor de x,y (evitamos pegarnos a
+//	distancia 0 de un muro) 
+int	aviable_position(double x, double y, t_world *world)
+{
+	if (is_wall(x + PLAYER_RADIUS, y, world->map) ||
+		is_wall(x + PLAYER_RADIUS, y + PLAYER_RADIUS, world->map) ||
+		is_wall(x, y + PLAYER_RADIUS, world->map) ||
+		is_wall(x - PLAYER_RADIUS, y + PLAYER_RADIUS, world->map) ||
+		is_wall(x - PLAYER_RADIUS, y, world->map) ||
+		is_wall(x - PLAYER_RADIUS, y - PLAYER_RADIUS, world->map) ||
+		is_wall(x, y - PLAYER_RADIUS, world->map)||
+		is_wall(x + PLAYER_RADIUS, y - PLAYER_RADIUS, world->map))
+			return (1);
+	return (0);
+}
+
+void	set_position(double new_x, double new_y, t_world *world)
+{
+	if (!aviable_position(new_x, new_y, world))
 	{
-		if (new_position_x - (int)new_position_x > 0.5)
-			margin_x = new_position_x + 0.04;
-		else
-			margin_x = new_position_x - 0.04;
-		if (new_position_y - (int)new_position_y > 0.5)
-			margin_y = new_position_y + 0.04;
-		else
-			margin_y = new_position_y - 0.04;
-		if (world->map[(int)(new_position_y)][(int)(new_position_x)] == '0' //new_position es un camino?
-			&& world->map[(int)(margin_y)][(int)(margin_x)] == '0'			//caso especial para esquinas
-			&& world->map[(int)(margin_y)][(int)(new_position_x)] == '0'	//tengo el margen minimo en el eje Y?
-			&& world->map[(int)(new_position_y)][(int)(margin_x)] == '0')	//tengo el margen minimo en el eje X?
-		{
-			world->char_position.x = new_position_x;
-			world->char_position.y = new_position_y;
-		}
+		world->char_position.x = new_x;
+		world->char_position.y = new_y;
 	}
+	else if (!aviable_position(new_x, world->char_position.y, world))
+		world->char_position.x = new_x;
+	else if (!aviable_position(world->char_position.x, new_y, world))
+		world->char_position.y = new_y;
 }
 
 void	rotate_vector(t_vector *v, double sinv, double cosv)
 {
 	double	nx;
 	double	ny;
-
+	
 	nx = v->x * cosv - v->y * sinv;
 	ny = v->x * sinv + v->y * cosv;
 	v->x = nx;
 	v->y = ny;
 }
 
-/*esta es la formula para rotar en sentido horario un vector
-x' = x * cos(θ) - y * sin(θ)
-y' = x * sin(θ) + y * cos(θ)*/
 void	rotate_normalize_vector(t_vector *vector, double sin, double cos)
 {
 	double	result_x;
 	double	result_y;
 	double	magnitude;
-
+	
 	result_x = vector->x * cos - vector->y * sin;
 	result_y = vector->x * sin + vector->y * cos;
 	magnitude = sqrt(result_x * result_x + result_y * result_y);
